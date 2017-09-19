6cb5b9e3a13b133d338ccef7bfd275ebc1e79167
Gabriel-Andrew Pollo-Guilbert <gabriel14_wii@hotmail.com>
2017-09-19 19:05:56 -0400

    Use #ifndef rather than #pragma

--- TP1/src/Const.h
+++ TP1/src/Const.h
@@ -1,6 +1,9 @@
-#pragma once
+#ifndef CONST_H
+#define CONST_H
 
 typedef unsigned int uint_t;
 
 const int MIN_CONCENTRATION_COULEUR = 0;
 const int MAX_CONCENTRATION_COULEUR = UINT8_MAX;
+
+#endif CONST_H
