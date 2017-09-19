3de7f06ea37a4bc0de29bc74e3a69bf0a65ba831
Gabriel-Andrew Pollo-Guilbert <gabriel14_wii@hotmail.com>
2017-09-19 18:57:07 -0400

    Implement Image class

--- TP1/src/Image.cpp
+++ TP1/src/Image.cpp
@@ -1,70 +1,174 @@
+#include <stdio.h>
+#include <assert.h>
+
 #include "Image.h"
-//test
-Image::Image()
-{
-    nomImage_ = "";
-    nombrePixelEnHauteur_ = 0;
-    nombrePixelEnLargeur_ = 0;
+
+Image::Image() {
+    name_   = "";
+    height_ = 1;
+    width_  = 1;
+    pixels_ = NULL;
+
+    allocatePixels();
 }
 
-Image::~Image()
-{
+Image::~Image() {
+    freePixels();
 }
 
-Image::Image(const string & nomImage, unsigned int nombrePixelHauteur, unsigned int nombrePixelLargeur)
-{
-    nomImage_ = nomImage;
-    nombrePixelEnHauteur_ = nombrePixelHauteur;
-    nombrePixelEnLargeur_ = nombrePixelLargeur;
+Image::Image(const string& name, uint_t height, uint_t width) {
+    name_   = name;
+    height_ = height;
+    width_  = width;
+    pixels_ = NULL;
 
+    allocatePixels();
 }
 
-void Image::doublerTailleEnLargeur()
-{
-    nombrePixelEnLargeur_ = nombrePixelEnLargeur_ * 2;
+Image::Image(const Image& image) {
+    name_   = image.getName();
+    height_ = image.getHeight();
+    width_  = image.getWidth();
+    pixels_ = NULL;
+
+    allocatePixels();
+    copyPixels(image);
 }
 
-void Image::doublerTailleEnHauteur()
-{
-    nombrePixelEnHauteur_ = nombrePixelEnHauteur_ * 2;
+void Image::doubleHeight() {
+    reallocatePixels(2*height_, width_);
 }
 
-unsigned int Image::obtenirNombrePixelHauteur() const
-{
-    return nombrePixelEnHauteur_;
+void Image::doubleWidth() {
+    reallocatePixels(height_, 2*width_);
 }
 
-unsigned int Image::obtenirNombrePixelLargeur() const
-{
-    return nombrePixelEnLargeur_;
+uint_t Image::getHeight() const {
+    return height_;
 }
 
-string Image::obtenirNomImage() const
-{
-    return nomImage_;
+uint_t Image::getWidth() const {
+    return width_;
 }
 
-void Image::modifierNomImage(const string & nomImage)
-{
-    nomImage_ = nomImage;
+string Image::getName() const {
+    return name_;
 }
 
-void Image::afficherImage() const
-{
-
+void Image::setName(const string& name) {
+    name_ = name;
 }
 
-bool Image::ajouterPixel(Pixel & pixel, unsigned int positionLargeur, unsigned int positionHauteur)
-{
-    return false;
+void Image::printImage() const {
+    printf("%s:\n", name_.c_str());
+    for(uint_t j = 0; j < height_; j++) {
+        for(uint_t i = 0; i < width_; i++) {
+            pixels_[j][i].printPixel();
+            printf(" ");
+        }
+        printf("\n");
+    }
 }
 
-Pixel Image::obtenirPixel(unsigned int positionHauteur, unsigned int positionLargeur) const
-{
-    return Pixel();
+bool Image::addPixel(Pixel& pixel, uint_t x, uint_t y) {
+    /* make sure the desired location is valid */
+    if(x >= width_ || y >= height_)
+        return false;
+
+    /* replace the pixel */
+    pixels_[y][x] = pixel;
+
+    return true;
 }
 
-void Image::augmenterTeintePixel(unsigned int positionLargeur, unsigned int positionHauteur, int increment, char couleur)
-{
+Pixel Image::getPixel(uint_t x, uint_t y) const {
+    /* make sure the desired location is valid */
+    if(x >= width_ || y >= height_)
+        return Pixel();
 
+    /* get the pixel */
+    return pixels_[y][x];
+}
+
+void Image::incrementValue(uint_t x, uint_t y, int value, char color) {
+    /* make sure the desired location is valid */
+    if(x >= width_ || y >= height_)
+        return;
+
+    /* increment the specified color */
+    switch(color) {
+        case 'r':
+        case 'R':
+            pixels_[y][x].incrementRed(value);
+            break;
+        case 'g':
+        case 'G':
+            pixels_[y][x].incrementGreen(value);
+            break;
+        case 'b':
+        case 'B':
+            pixels_[y][x].incrementBlue(value);
+            break;
+    }
+}
+
+void Image::allocatePixels() {
+    /* this method shouldn't be called with pixels data allocated */
+    assert(pixels_ == NULL);
+
+    /* allocate the memory for the pixels */
+    pixels_ = new Pixel*[height_];
+    for(uint_t j = 0; j < height_; j++)
+        pixels_[j] = new Pixel[width_];
+}
+
+void Image::reallocatePixels(uint_t height, uint_t width) {
+    /* this method shouldn't be called without pixels data allocated */
+    assert(pixels_ != NULL);
+
+    /* allocate the new memory for the pixels */
+    Pixel** temp = new Pixel*[height];
+    for(uint_t j = 0; j < height; j++)
+        temp[j] = new Pixel[width];
+
+    /* find the limits of the old pixel data */
+    uint_t minx = width < width_ ? width : width_;
+    uint_t miny = height < height_ ? height : height_;
+
+    /* copy the old pixel data that fits into the new image */
+    for(uint_t j = 0; j < miny; j++)
+        for(uint_t i = 0; i < minx; i++)
+            temp[j][i] = pixels_[j][i];
+
+    /* free the old pixels data */
+    freePixels();
+
+    /* update the attributes of the object */
+    width_  = width;
+    height_ = height;
+    pixels_ = temp;
+}
+
+void Image::freePixels() {
+    /* this method shouldn't be called without pixels data allocated */
+    assert(pixels_ != NULL);
+
+    /* free the memory of the pixels */
+    for(uint_t j = 0; j < height_; j++)
+        delete [] pixels_[j];
+    delete [] pixels_;
+
+    /* remove the pointer */
+    pixels_ = NULL;
+}
+
+void Image::copyPixels(const Image& image) {
+    /* make sure there is enough pixels */
+    assert(width_ == image.getWidth());
+    assert(height_ == image.getHeight());
+
+    /* copy the pixels */
+    for(uint_t j = 0; j < height_; j++)
+        for(uint_t i = 0; i < width_; i++)
+            pixels_[j][i] = image.getPixel(i, j);
 }
--- TP1/src/Image.h
+++ TP1/src/Image.h
@@ -3,41 +3,171 @@
 
 #include "Pixel.h"
 #include "Const.h"
+
 #include <string>
-#include <iostream>
 
 using namespace std;
 
 class Image
 {
 public:
+    /**
+     * Default constructor.
+     */
     Image();
+
+    /**
+     * Destructor.
+     */
     ~Image();
 
-    Image(const string& nomImage, uint8_t nombrePixelHauteur, uint8_t nombrePixelLargeur);
+    /**
+     * Overloaded constructor.
+     *
+     * @param name   The name of the image.
+     * @param height The height of the image.
+     * @param width  The width of the image.
+     */
+    Image(const string& name, uint_t height, uint_t width);
 
-    void doublerTailleEnLargeur();
-    void doublerTailleEnHauteur();
+    /**
+     * Copy constructor.
+     *
+     * @param image The object to copy from.
+     */
+    Image(const Image& image);
 
-    uint_t obtenirNombrePixelHauteur() const;
-    uint_t obtenirNombrePixelLargeur() const;
-    string obtenirNomImage() const;
+    /**
+     * Copy constructor.
+     *
+     * @param image The image to copy.
+     */
+     //Image(const Image& image);
 
-    void modifierNomImage(const string & nomImage);
+    /**
+     * This method doubles the height of the image.
+     */
+    void doubleHeight();
 
-    void afficherImage() const;
+    /**
+     * This method doubles the width of the image.
+     */
+    void doubleWidth();
 
-    bool ajouterPixel(Pixel & pixel,unsigned int positionLargeur, unsigned int positionHauteur);
-    Pixel obtenirPixel(unsigned int positionHauteur, unsigned int positionLargeur) const ;
-    void augmenterTeintePixel(unsigned int positionLargeur, unsigned int positionHauteur, int  increment, char couleur);
+    /**
+     * This method returns the height of the image.
+     *
+     * @return The height of the image.
+     */
+    uint_t getHeight() const;
 
+    /**
+     * This method returns the width of the image.
+     *
+     * @return The width of the image.
+     */
+    uint_t getWidth() const;
+
+    /**
+     * This method returns the name of the image.
+     *
+     * @return The name of the image.
+     */
+    string getName() const;
+
+    /**
+     * This method sets the name of the image.
+     *
+     * @param name The new name of the image.
+     */
+    void setName(const string& name);
+
+    /**
+     * This method prints the image.
+     */
+    void printImage() const;
+
+    /**
+     * This method adds a Pixel to the specified location.
+     *
+     * @param pixel The pixel to add.
+     * @param x     The x location of the pixel.
+     * @param y     The y location of the pixel.
+     *
+     * @return True if the pixel was succesfully added, else False if the
+     *         specified location is out of the image.
+     */
+    bool addPixel(Pixel& pixel, uint_t x, uint_t y);
+
+    /**
+     * This method returns the specified pixel.
+     *
+     * @param x The x location of the pixel.
+     * @param y The y location of the pixel.
+     *
+     * @return A pointer to the specified pixel, else a NULL pointer if the
+     *         specified location is out of the image.
+     */
+    Pixel getPixel(uint_t x, uint_t y) const;
+
+    /**
+     * This method increments the color value of the pixel at the specified
+     * location.
+     *
+     * @param x     The x location of the pixel.
+     * @param y     The y location of the pixel.
+     * @param value The value to add to the color.
+     * @param color The color to modify, either 'r', 'g' or 'b'.
+     */
+    void incrementValue(uint_t x, uint_t y, int value, char color);
 
 private :
-    string nomImage_;
-    unsigned int nombrePixelEnHauteur_;
-    unsigned int nombrePixelEnLargeur_;
+    /**
+     * The name of the image.
+     */
+    string name_;
+
+    /**
+     * The height of the image.
+     */
+    uint_t height_;
+
+    /**
+     * The width of the image.
+     */
+    uint_t width_;
+
+    /**
+     * The pixels data of the image.
+     */
     Pixel** pixels_;
 
+    /**
+     * This method allocates the memory for the pixels.
+     */
+    void allocatePixels();
+
+    /**
+     * This method reallocates the memory for the pixels. In the same way that
+     * realloc() keeps the old data from malloc(), this method keeps the old
+     * pixels that still fit in the new image.
+     *
+     * @param height The new height of the image.
+     * @param width  The new width of the image.
+     */
+    void reallocatePixels(uint_t height, uint_t width);
+
+    /**
+     * This method frees the memory of the pixels.
+     */
+    void freePixels();
+
+    /**
+     * This method copies all the pixels from another image.
+     *
+     * @param image The image to copy pixels from.
+     */
+    void copyPixels(const Image& image);
 };
 
 #endif
