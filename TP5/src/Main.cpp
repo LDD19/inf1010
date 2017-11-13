fe9097fa0f6b269f388b6dc6bdf73388248f7179
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2017-11-12 20:43:59 -0500

    Implement Main

--- TP5/src/Main.cpp
+++ TP5/src/Main.cpp
@@ -1,4 +1,3 @@
-
 #ifndef PAUSE
     #ifdef _WIN32
         #include <Windows.h>
@@ -7,155 +6,188 @@
         #include <unistd.h>
         #define PAUSE sleep(0)
     #endif
-#endif /* PAUSE */
+#endif
 
 #include <iostream>
 #include <string>
+#include <queue>
+#include <functional>
 
 #include "GroupeImage.h"
-#include <queue>
-//#include "BaseDeDonnesSpecifique.h"
-//#include "Foncteur.h"
-#include <functional>
+#include "BaseDeDonnesSpecifique.h"
+#include "Foncteur.h"
 
 using namespace  std;
 
 int main() {
+    /* chemin vers les images */
+    string base = "./res/";
+    string originale = base + "Originale/";
+
+    /* nom des 6 images */
+	string noms[9] = {
+        "Breaking-Bad.bmp",
+        "Couleur.bmp",
+        "Fall.bmp",
+        "RM.bmp",
+        "SolarEclipse.bmp",
+        "Monkey.bmp",
+        "WiC.bmp",
+        "Sloth.bmp",
+        "Green.bmp"
+    };
+
+    /* on crée une file avec les images */
+    queue<Image*> images;
+    for(string nom : noms) {
+        Image* image = new Image(originale+nom, TypeImage::Couleurs);
+        images.push(image);
+    }
+
+    /* on crée 3 groupes d'images */
+	GroupeImage groupe1;
+	GroupeImage groupe2;
+	GroupeImage groupe3;
+
+    /* on crée une base de données */
+    BaseDeDonnesSpecifique db;
+
+    /* on y ajoute les 3 groupes */
+    db += &groupe1;
+    db += &groupe2;
+    db += &groupe3;
+
+    /* on ajoute les éléments de manière uniforme aux groupes */
+	cout << "\tCHARGEMENT DES IMAGES\n" << endl;
+    while(!images.empty()) {
+        static int cntr = 1;
+
+        /* on choisit à quel groupe il faut l'ajouter */
+        Image* i = images.front();
+        switch(cntr++) {
+            case 1: db.associerImage(&groupe1, i); break;
+            case 2: db.associerImage(&groupe2, i); break;
+            case 3: db.associerImage(&groupe3, i); cntr = 1; break;
+        }
+
+        /* on enlève l'élèment de la file*/
+        images.pop();
+    }
+	cout << endl;
 	
-	// Chemin vers les images
-	string cheminLoad = "./Ensemble d'images/Originale/";
-    // Nom des 6 images dans un tableau de string
-	string image[9] = {"Breaking-Bad.bmp", "Couleur.bmp", "Fall.bmp", "RM.bmp", "SolarEclipse.bmp", "Monkey.bmp","WiC.bmp","Sloth.bmp","Green.bmp"};
-
-    // Creer un conteneur de type QUEUE et ajouter les images dans la queue
-	cout << "**************************************************" << endl;
-	cout << "Creation de la file de travail" << std::endl;
-	cout << "**************************************************" << endl << endl;
+    /* on affiche la base de données au complet */
+	cout << "\tAFFICHAGE DE LA BASE DE DONNÉES\n" << endl;;
+    cout << db;
 
 
-	//Creez 3 GroupeImage
+    /* on convertit une image au hasard dans chaque groupe */
+    cout << "\tCONVERSION D'IMAGES\n" << endl;
+    FoncteurRandom r(0, 2);
+
+    /* on convertit une image du groupe 1 en négatif */
+    groupe1.obtenirImage(r())->toutMettreEnNegatif();
+
+    /* on convertit une image du groupe 2 en gris */
+    groupe2.obtenirImage(r())->convertirGris();
+
+    /* on convertit une image du groupe 3 en noir et blanc */
+    groupe3.obtenirImage(r())->convertirNB();
 	
+	/* TRAITEMENT DU GROUPE 1 */
+	cout << "\n\tCALCUL DES MOYENNES\n" << endl;
 
-	// Creez un objet de type BaseDeDonnesSpecifique et ajoutez les 3 groupes a la base de donnes
-	
+    /* on affiche celle du groupe 1 */
+    double moy = groupe1.obtenirIntensiteMoyenne();
+    cout << "L'intensité moyenne du groupe 1 est de " << moy << "." << endl;
+    cout << endl;
 
+    /* on affiche toutes les images avec une intensité inférieure  */
+    for(Image* image : db.obtenirListeImages()) {
+        double m = image->retournerIntensiteMoyenne();
+        string nom = image->obtenirNomImage();
 
-	/*Faites repartir les elements de la queue de maniere uniforme et sequentielle dans les groupe
-	 Vous devez avoir la composition suivante :
-		groupe 1 a les images 0,3,6
-		groupe 2 a les images 1,4,7
-		groupe 3 a les images 2,5,8
-	 ATTENTION : la repartition doit se faire dans une seule boucle ET vous devez utiliser les fonctions de l'objet de base de donnes
-	 NB: La methode pop() de queue<> ne retourne pas l'element de la structure
-	*/
-	
-	cout << "**************************************************" << endl;
-	cout << "CHARGEMENT DES IMAGES\n\n";
-	// TODO : Votre implementation ici
+        if(image->retournerIntensiteMoyenne() < moy)
+            cout << "L'intensité moyenne de l'image " << nom << " est de " << m << "." << endl;
+    }
 
-	
-	cout << "**************************************************" << endl << endl;
-	cout <<"AFFICHAGE DE LA BASE DE DONNESES\n";
-	// TODO : Afficher la base de donnes au complet
+	/* TRAITEMENT DU GROUPE 2 */
+	cout << "\n\tCALCUL DE LA TAILLE\n" << endl;
 
-	// Pour chaque groupe, generez un nombre aleatoire. Le nombre aleatoire doit toujours donner un index valide
-	// ET appliquez les modifications suivantes aux images obtenues avec le nombre aleatoire
-	//GROUPE 1 - Mettre en negatif
-	
+    /* on affiche celle du groupe 2 */
+    moy = groupe2.obtenirTailleMoyenne();
+    cout << "La taille moyenne du groupe 2 est de " << moy << "." << endl;
 
+    /* on affiche le nombre d'images dans la base de données */
+    uint_t taille = db.obtenirListeImages().size();
+    cout << "Il y a " << taille << " images dans la base de données." << endl;
 
-	//GROUPE 2 - Mettre en gris
-	
+    /*
+     * Trouver et afficher le nombre d'images qui ont une tailles plus grande que
+     * la moyenne calculée. Utiliser les fonctions std::bind et std::placeholders
+     * pour effectuer le travail.
+     *
+     * FIXME: why the fuck on utiliserait ces fonctions?
+     */
 
+    /* on compte les images */
+    list<Image*> liste = db.obtenirListeImages();
+    uint_t nb = count_if(liste.begin(), liste.end(),
+        [&moy](Image* img) {
+            return (img->obtenirTaille() > moy);
+        }
+    );
+    cout << "Il y a " << nb << " images plus grandes que la moyenne." << endl;
 
-	//GROUPE 3 - Mettre en noir et blanc
-	
+    /*
+     * FIXME: Je ne comprend pas pourquoi ils nous font faire ça, on veut le nombre
+     * d'images totales dont la est taille plus grande que la moyenne. Là, ils nous
+     * font juste compter celles fesant partie du groupe 2...
+     */
 
+    groupe2.supprimerElements(
+        bind(
+            [](double t, double m) {
+                return (t < m);
+            },
+            bind(
+                FoncteurObtenirTailleImage(),
+                placeholders::_1
+            ),
+            moy
+        )
+    );
 
+	/* TRAITEMENT DU GROUPE 3 */
+    cout << "\n\tCONVERSION EN NOIR ET BLANC\n" << endl;
 
-	/*
-	 *TRAITEMENT DE GROUPE 1
-	 *Calculer l'intensite moyenne des images du groupe 1 et l'afficher
-	 */
+    /* on convertit les images */
+    groupe3.appliquerFoncteurUnaire(FoncteurMettreEnBN());
 
-	cout << "**************************************************" << endl;
-	cout << "CALCUL DE LA MOYENNE \n\n";
-	// TODO : Votre implementation ici
+    cout << "\n\tENREGISTREMENT DES IMAGES\n" << endl;
 
+    /* on enregistre toutes les images */
+    groupe3.toutEnregistrer(base);
 
-	cout << "MOYENNES DES IMAGES DANS LA BASE DE DONNES: \n";
-
-
-	// Trouver toutes les images avec une intensite moyenne plus petite que la valeur trouve et
-	// afficher leur nom et leur moyenne
-	// ATTENTION : Faire ceci avec une boucle for intelligente de type 
-	//																for (Type variable : conteneur)
-
-
-	cout << "**************************************************" << endl << endl;
-
-
-
-	cout << "**************************************************" << endl;
-	cout << "CALCUL DE LA TAILLE \n\n";
-	// TODO : Votre implementation ici
-
-	/*
-	 *TRAITEMENT DE GROUPE 2
-	 * Calculer la taille moyenne des images dans le groupe 2 et afficher l'afficher
-	 */
-	
-
-
-	// Afficher le nombre d'images dans la base de donnes
-//	cout << "Nombre d'images totales dans la base de donnes " << dataBase.obtenirListImages().size() << endl;
-
-	// Trouver at afficher le nombre d'images qui ont une taille plus grande que la moyenne calculee
-	// Utiliser les fonctions std::bind et std::placeholders pour effectuer le travail
-
-	// Decommenter et Remplacer les variables X,Y, Z par les bons elements 
-	/*
-	 MonFoncteur foncteurTaille;
-	 groupeImage2->supprimerElements(
-		std::bind(
-			X, //A COMPLETER générer un opérateur > pour les entiers
-			//Le bind ci-dessous permet d'appeler la méthode obtenirTaile de la
-			//Image* passée en argument du foncteur lors des appels de ce dernier dans supprimerElements
-			std::bind(
-				foncteurTaille,
-				Y //A COMPLETER utiliser le premier placeholder (attention aux namespace) (reçoit une Image*)
-				),
-			Z //A COMPLETER comparer à taille Moyenne
-			)
-		);
-	 */
-
-	// Calculer le nombre d'images avec une taille superieure a la taille moyenne
-	// ASTUCE: utilisez la STL et les fonctions de decompte conditionel
-
-	// Afficher le nombre d'images dans la base de donnes avec une taille plus grande que la moyenne
-	cout << "Nombre d'images dont la taille est plus grande que la moyenne " << "REMPLACEZ CA AVEC LA TAILLE" << endl;
-	
-	cout << "**************************************************" << endl << endl;
-
-
-	/*
-	 *TRAITEMENT DU GROUPE 3
-	 *
-	 *Mettre tout les images du groupe 3 en noir et blanc et sauvegarder
-	 *ATTENTION : Vous devez utiliser la fonction generique "appliquerFoncteurUnaire"  demandee dans au TP5 et non une boucle sur les elements
-	 */
-
-	cout << "**************************************************" << endl;
-	cout << "CONVERSION EN NOIR ET BLANC \n";
-	// TODO : Votre implementation ici
-
-	cout << "**************************************************" << endl << endl;
-
-
-    // Permet d'afficher le contenu de la console
+    /* permet d'afficher la console sous Windows */
     PAUSE;
 
-    // Fin du programme
+    /* on libère la mémoire */
+    for_each(liste.begin(), liste.end(),
+        [](Image* img) {
+            delete img;
+        }
+    );
+
+    /*
+     * En executant `valgrind`, soit
+     *
+     * $ make
+     * $ valgrind ./build/image
+     * 
+     * on obtient "All heap blocks were freed -- no leaks are possible". Par
+     * conséquent, il n'y a aucune fuite de mémoire dans notre programme.
+     */
+
+    /* fin du programme */
     return 0;
 }
